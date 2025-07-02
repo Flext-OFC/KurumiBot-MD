d64b1fd5c70475cb8318a4bbf70bbeb2605d4700
Flext-OFC <flextofc@gmail.com>
2025-07-02 14:05:07 -0300

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -96,8 +96,8 @@ global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo
 }}, { quoted: m }
 
 global.icono = [
-'https://qu.ax/Dfpfh.jpg',
-'https://qu.ax/bEUcE.jpg'
+'https://files.catbox.moe/oyj83o.jpg',
+'https://files.catbox.moe/o2s23u.jpg'
 ].getRandom()
 
 global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: packname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
